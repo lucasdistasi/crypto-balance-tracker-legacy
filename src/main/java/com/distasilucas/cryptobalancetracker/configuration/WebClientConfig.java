41451fec0692a946263a4d76852d247f37851f78
Di Stasi Lucas <distasilucas@gmail.com>
2023-02-18 19:55:50 -0300

    CBT-021 - Added coingecko-pro properties

--- src/main/java/com/distasilucas/cryptobalancetracker/configuration/WebClientConfig.java
+++ src/main/java/com/distasilucas/cryptobalancetracker/configuration/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.distasilucas.cryptobalancetracker.configuration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,22 @@ public class WebClientConfig {
     @Value("${coingecko.url}")
     private String coingeckoUrl;
 
+    @Value("${coingecko.pro.url}")
+    private String coingeckoProUrl;
+
+    @Value("${coingecko.pro.api-key}")
+    private String coingeckoApiKey;
+
     @Bean
     public WebClient coingeckoWebClient() {
+        String baseUrl = StringUtils.isNotBlank(coingeckoApiKey) ?
+                coingeckoProUrl :
+                coingeckoUrl;
+
         return WebClient.builder()
                 .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs()
                         .maxInMemorySize(700 * 1024))
-                .baseUrl(coingeckoUrl)
+                .baseUrl(baseUrl)
                 .build();
     }
 }
